986661894668f74f6551b20cf46b947f848d534b
mhmdsghyrmhsnahmdshryf739-dot <mhmdsghyrmhsnahmdshryf739@gmail.com>
2026-06-03 16:36:53 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('BOT_TOKEN', getenv('BOT_TOKEN'));
+define('ADMIN_ID', getenv('ADMIN_ID'));
+define('API_ID', getenv('API_ID'));
+define('API_HASH', getenv('API_HASH'));
+define('DB_PATH', __DIR__ . '/numbers.db');
+define('SESSIONS_PATH', __DIR__ . '/sessions/');
+?>
